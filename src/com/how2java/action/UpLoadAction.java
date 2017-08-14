940d932e33082de1c4ce96bb17198b22100af45e
wangyanchao <wangyanchao@DESKTOP-6DUR8O3>
2017-08-14 11:09:32 +0800

    new

---
+++ src/com/how2java/action/UpLoadAction.java
@@ -0,0 +1,33 @@
+package com.how2java.action;
+
+import java.io.File;
+
+public class UpLoadAction {
+	File doc;
+    String docFileName;
+    String docContentType;
+	public File getDoc() {
+		return doc;
+	}
+	public void setDoc(File doc) {
+		this.doc = doc;
+	}
+	public String getDocFileName() {
+		return docFileName;
+	}
+	public void setDocFileName(String docFileName) {
+		this.docFileName = docFileName;
+	}
+	public String getDocContentType() {
+		return docContentType;
+	}
+	public void setDocContentType(String docContentType) {
+		this.docContentType = docContentType;
+	}
+	  public String upload() {
+	        System.out.println(doc);
+	        System.out.println(docFileName);
+	        System.out.println(docContentType);
+	        return "success";
+	    }
+}
---
+++ src/com/how2java/bean/Category.java
@@ -0,0 +1,29 @@
+package com.how2java.bean;
+
+import java.util.List;
+import java.util.Set;
+
+public class Category {
+	private int id;
+	private String name;
+	private List<Product> products;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
+}
